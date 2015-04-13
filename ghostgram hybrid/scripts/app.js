71289abbf882967aad1f238d18631519d53767a1
don bradford <donbrad@hotmail.com>
2015-04-13 19:23:28 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/app.js
+++ ghostgram hybrid/scripts/app.js
@@ -8,6 +8,7 @@
     // create an object to store the models for each view
     window.APP = {
       models: {
+       
         home: {
           title: 'ghostgrams',
           privateMode: false
@@ -43,11 +44,20 @@
 
     // this function is called by Cordova when the application is loaded by the device
     document.addEventListener('deviceready', function () {  
-      
+      var initialView = '#newuserhome';
+        
       // hide the splash screen as soon as the app is ready. otherwise
       // Cordova will wait 5 very long seconds to do it for you.
       navigator.splashscreen.hide();
 
+        Parse.initialize("lbIysFqoATM1uTxebFf5s8teshcznua2GQLsx22F", "MmrJS8jR0QpKxbhS2cPjjxsLQKAuGuUHKtVPfVj5");
+
+        Parse.User.enableRevocableSession();
+        APP.models.profile.currentUser = Parse.User.current();
+        
+        if (APP.models.profile.currentUser)
+            initialView = '#home';
+            
       APP.kendo = new kendo.mobile.Application(document.body, {
         
         // comment out the following line to get a UI which matches the look
@@ -55,16 +65,11 @@
         skin: 'flat',
 
         // the application needs to know which view to load first
-        initial: 'views/home.html'
+        initial: initialView
       });
         
-        Parse.initialize("lbIysFqoATM1uTxebFf5s8teshcznua2GQLsx22F", "MmrJS8jR0QpKxbhS2cPjjxsLQKAuGuUHKtVPfVj5");
-
-        Parse.User.enableRevocableSession();
-        APP.models.profile.currentUser = Parse.User.current();
-       
         
-
+        
 
     }, false);
 
