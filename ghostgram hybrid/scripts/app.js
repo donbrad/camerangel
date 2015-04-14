b07f35496481d73e38da8f80d126c26d7d31dacf
don bradford <donbrad@hotmail.com>
2015-04-14 17:24:18 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/app.js
+++ ghostgram hybrid/scripts/app.js
@@ -17,7 +17,7 @@
         profile: {
           title: 'Profile',
           currentUser: '',
-          userName: '',
+          username: '',
           email: '',
           phone: '',
           emaiVerified: false,
@@ -35,41 +35,37 @@
           title: 'Contacts'
             
         }
-      }
+      },
+       kendo: ''
     };
 
 
 
     // this function is called by Cordova when the application is loaded by the device
     document.addEventListener('deviceready', function () {  
-      
+      var initialView = '#newuserhome';
+        
       // hide the splash screen as soon as the app is ready. otherwise
       // Cordova will wait 5 very long seconds to do it for you.
       navigator.splashscreen.hide();
 
-      app = new kendo.mobile.Application(document.body, {
+     Parse.initialize("lbIysFqoATM1uTxebFf5s8teshcznua2GQLsx22F", "MmrJS8jR0QpKxbhS2cPjjxsLQKAuGuUHKtVPfVj5");
+
+        Parse.User.enableRevocableSession();
+        APP.models.profile.currentUser = Parse.User.current();
+        
+        if (APP.models.profile.currentUser)
+            initialView = '#home';
+            
+      APP.kendo = new kendo.mobile.Application(document.body, {
         
         // comment out the following line to get a UI which matches the look
         // and feel of the operating system
         skin: 'flat',
 
         // the application needs to know which view to load first
-        initial: 'views/home.html'
+        initial: initialView
       });
-        
-        Parse.initialize("lbIysFqoATM1uTxebFf5s8teshcznua2GQLsx22F", "MmrJS8jR0QpKxbhS2cPjjxsLQKAuGuUHKtVPfVj5");
-        Parse.User.enableRevocableSession();
-        APP.models.profile.currentUser = Parse.User.current();
-       
-        if (APP.models.home.currentUser) {
-            // Have current user - hide signup and signin
-            $('#home-signin').addClass('hidden');
-             $('#home-status').removeClass('hidden');
-        } else {
-            // No current user - show signup
-            $('#home-signin').removeClass('hidden');
-             $('#home-status').addClass('hidden');
-        }
 
 
     }, false);
