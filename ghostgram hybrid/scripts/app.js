941c41ce8792afbf588520c17ff0d80191edd6d2
don bradford <donbrad@hotmail.com>
2015-04-16 20:19:28 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/app.js
+++ ghostgram hybrid/scripts/app.js
@@ -1,6 +1,13 @@
 
-(function () {
-
+(function ($, doc) {
+    var _app,
+    	_mapElem,
+    	_mapObj,
+    	_storeListElem,
+    	_private,
+    	_appData = new AppData(),
+    	_isOnline = true;
+    
     // create an object to store the models for each view
     window.APP = {
       models: {
@@ -23,19 +30,26 @@
        channels: {
           title: 'Channels',
           pubnub: '',
-          currentChannel: ''
+          currentChannel: '',
+          channelDS : ''
           //Todo: Add channel data source and sync if user is signed in
         },
           
         gallery: {
-          title: 'gallery'
+          title: 'gallery',
+          galleryDS: ''
           //Todo: Add photo gallery data source and sync if user is signed in
         },
           
         contacts: {
-          title: 'Contacts'
+          title: 'Contacts',
+          contactsDS: ''
            //Todo: Add contacts cache data source and sync if user is signed in
             
+        },
+        places: {
+            title: 'Places',
+            placesDS: ''
         }
       },
        kendo: ''
@@ -45,38 +59,38 @@
 
     // this function is called by Cordova when the application is loaded by the device
     document.addEventListener('deviceready', function () {  
-    var initialView = '#newuserhome';
+        var initialView = '#newuserhome';
 
-    // hide the splash screen as soon as the app is ready. otherwise
-    // Cordova will wait 5 very long seconds to do it for you.
-    navigator.splashscreen.hide();
+        // hide the splash screen as soon as the app is ready. otherwise
+       
+        navigator.splashscreen.hide();
 
-    Parse.initialize("lbIysFqoATM1uTxebFf5s8teshcznua2GQLsx22F", "MmrJS8jR0QpKxbhS2cPjjxsLQKAuGuUHKtVPfVj5");
+        Parse.initialize("lbIysFqoATM1uTxebFf5s8teshcznua2GQLsx22F", "MmrJS8jR0QpKxbhS2cPjjxsLQKAuGuUHKtVPfVj5");
 
-    Parse.User.enableRevocableSession();
-    APP.models.profile.currentUser = Parse.User.current();
+        Parse.User.enableRevocableSession();
+        APP.models.profile.currentUser = Parse.User.current();
 
-    if (APP.models.profile.currentUser) {
-         initialView = '#home';
-        APP.models.profile.username =  APP.models.profile.currentUser.attributes.username;
-        APP.models.profile.email =  APP.models.profile.currentUser.attributes.email;
-        APP.models.profile.phone =  APP.models.profile.currentUser.attributes.phone;
-        APP.models.profile.alias =  APP.models.profile.currentUser.attributes.alias;
-    }
+        if (APP.models.profile.currentUser) {
+             initialView = '#home';
+            APP.models.profile.username =  APP.models.profile.currentUser.attributes.username;
+            APP.models.profile.email =  APP.models.profile.currentUser.attributes.email;
+            APP.models.profile.phone =  APP.models.profile.currentUser.attributes.phone;
+            APP.models.profile.alias =  APP.models.profile.currentUser.attributes.alias;
+        }
 
-     APP.models.channels.pubnub = PUBNUB.init({ 
-         publish_key: 'pub-c-7344645a-12aa-4481-8ad8-01b2e29deba9', 
-         subscribe_key: 'sub-c-4866fe96-dcb2-11e4-8fb9-0619f8945a4f' 
-     });   
-        
-      APP.kendo = new kendo.mobile.Application(document.body, {
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'flat',
+         APP.models.channels.pubnub = PUBNUB.init({ 
+             publish_key: 'pub-c-7344645a-12aa-4481-8ad8-01b2e29deba9', 
+             subscribe_key: 'sub-c-4866fe96-dcb2-11e4-8fb9-0619f8945a4f' 
+         });   
 
-        // the application needs to know which view to load first
-        initial: initialView
+          APP.kendo = new kendo.mobile.Application(document.body, {
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: initialView
       });
     
      
---
+++ ghostgram hybrid/scripts/appData.js
@@ -0,0 +1,10 @@
+var AppData = function() {
+	var _endpoints,
+    	_initialCards,
+    	_announcements,
+        _private;
+    
+    return {
+        
+    };
+}
