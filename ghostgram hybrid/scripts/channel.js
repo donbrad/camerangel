b7e75dcfad16ac79b2c8006451283203926a3e26
don bradford <donbrad@hotmail.com>
2015-05-06 05:49:22 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/channel.js
+++ ghostgram hybrid/scripts/channel.js
@@ -13,3 +13,16 @@ function onShowChannel(e) {
 	 $("#channelNavBar").kendoMobileNavBar("title", name);
 	
 }
+
+function AutoGrowTextArea(textField)
+{
+  if (textField.clientHeight < textField.scrollHeight)
+  {
+    textField.style.height = textField.scrollHeight + "px";
+    if (textField.clientHeight < textField.scrollHeight)
+    {
+      textField.style.height = 
+        (textField.scrollHeight * 2 - textField.clientHeight) + "px";
+    }
+  }
+}
