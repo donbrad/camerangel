e21369ae639112b84cc81edce72c8a96bd5b0f3d
don bradford <donbrad@hotmail.com>
2015-05-14 02:41:53 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/utils.js
+++ ghostgram hybrid/scripts/utils.js
@@ -71,20 +71,14 @@ function getBase64FromImageUrl(URL, callback) {
     var img = new Image();
     img.src = URL;
     img.onload = function () {
+		var canvas = document.createElement("canvas");
+		canvas.width =this.width;
+		canvas.height =this.height;
 
-
-    var canvas = document.createElement("canvas");
-    canvas.width =this.width;
-    canvas.height =this.height;
-
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(this, 0, 0);
-
-
-    var dataURL = canvas.toDataURL("image/png");
-
-    callback(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(this, 0, 0);
+		var dataURL = canvas.toDataURL("image/png");
+		callback(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
     }
 }
 
