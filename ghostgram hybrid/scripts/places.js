a399dacba114e1c45e803f93a8217b94a13cc332
don bradford <donbrad@hotmail.com>
2015-05-18 19:38:13 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/places.js
+++ ghostgram hybrid/scripts/places.js
@@ -6,7 +6,7 @@ function onInitPlaces(e) {
             var place = e.dataItem;
            APP.models.places.currentPlace = place;
 			
-           $("#modalview-addPlace").kendoMobileModalView("open");
+         
         }
      });	
 }
@@ -22,14 +22,18 @@ function onInitFindPlace(e) {
    
 
   	google.maps.event.addListener(APP.models.places.autocomplete, 'place_changed', onPlaceChanged);
+	
      $("#findplace-listview").kendoMobileListView({
         dataSource: APP.models.places.geoPlacesDS,
         template: $("#geoPlacesTemplate").html(),
         click: function (e) {
             var place = e.dataItem;
+			place.lat = APP.location.position.coords.latitude;
+			place.lng = APP.location.position.coords.longitude;
 			// Todo: add lat/lng for this location
            APP.models.places.currentGeoPlace = place;
-			
+			$('#addPlaceAddress').val(place.formatted_address);
+			  $("#modalview-addPlace").kendoMobileModalView("open");
            
         }
      });
@@ -38,6 +42,20 @@ function onInitFindPlace(e) {
 function onPlaceChanged() {
 	var place = autocomplete.getPlace();
  	APP.models.places.currentGeoPlace = place;
+	
+	APP.map.geocoder.geocode( { 'address': place.formatted_address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        
+		  APP.models.places.currentGeoPlace.lat = results[0].geometry.location[0];
+		  APP.models.places.currentGeoPlace.lng = results[0].geometry.location[1];
+		  $('#addPlaceAddress').val(place.formatted_address);
+		   $("#modalview-addPlace").kendoMobileModalView("open");
+      
+      } else {
+        mobileNotify("Geocode was not successful for the following reason: " + status);
+      }
+    });
+	
 
 }
 
@@ -51,7 +69,9 @@ function onInitAddPlace(e) {
 }
 
 function onShowAddPlace(e) {
-	var place = APP.models.places.geoPlacesDS;
+	e.preventDefault();
+	
+	var place = APP.models.places.currentGeoPlace;
 	
 	$('#addPlaceAddress').val(place.formatted_address);
 	
@@ -60,21 +80,22 @@ function onShowAddPlace(e) {
 function addPlaceAdd(e) {
 	 var Places = Parse.Object.extend("places");
     var place = new Places();
-	var uuid = guid.v4();
+	var uuid = uuid.v4();
 	
 	place.set('uuid', uuid);
 	place.set('name', $('#addPlaceName').val());
 	place.set('address', $('#addPlaceAddress').val());
 	place.set('privacy', $('#addPlacePrivacy').val());
 	place.set('visible', $('#addPlaceVisible').val());
-	place.set('lat', APP.location.position.coord.latitude);
-	place.set('lng', APP.location.position.coord.longitude);
+	place.set('lat',  APP.models.places.currentGeoPlace.lat);
+	place.set('lng', APP.models.places.currentGeoPlace.lng);
 	
 	place.save(null, {
 	  success: function(place) {
 		// Execute any logic that should take place after the object is saved.
 		mobileNotify('Added place : ' + place.get('name'));
 		APP.models.places.placesDS.add(place.attributes);
+		  $("#modalview-addPlace").kendoMobileModalView("close");
 		APP.kendo.navigate('#places');
 	  },
 	  error: function(contact, error) {
