f466d7a1a163eecfe2c48c272ee24a9ea281d2e6
don bradford <donbrad@hotmail.com>
2015-05-23 19:39:28 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/gallery.js
+++ ghostgram hybrid/scripts/gallery.js
@@ -2,14 +2,25 @@ function onInitGallery(e){
     e.preventDefault();
     // ToDo: Initialize list view
     
-     $("#gallery-listview").kendoMobileListView({
-        dataSource: APP.models.gallery.galleryDS,
-        template: $("#gallery-listview-template").html(),
-        filterable: {
-            field: "name",
-            operator: "startswith"
-        },
-        endlessScroll: true
-    }); 
+     $('#gallery-grid').isotope({
+	  // options
+		  itemSelector: '.gallery-item',
+		  layoutMode: 'packery'
+	});
+	}
+
+
+function onShowGallery(e) {
+	e.preventDefault();
+	var grid = $('#gallery-grid');
+	//Clear out previous content
+	grid.empty();
+	
+	var photoArray = APP.models.gallery.photosDS.data();
+	
+	for (var i=0; i< photoArray.length; i++) {
+		grid.append('<div class="gallery-item"> <img id=" + photoArray[i].photoId + '" src="' + photoArray[i].thumb + '"/> </div>');
+	}
+	
 }
 
