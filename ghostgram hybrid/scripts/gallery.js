5d2015a339f76ae083ca01113aba2f92aae8926f
don bradford <donbrad@hotmail.com>
2015-05-23 22:05:06 +0000

    Changes made by don bradford (donbrad@hotmail.com).

--- ghostgram hybrid/scripts/gallery.js
+++ ghostgram hybrid/scripts/gallery.js
@@ -1,26 +1,36 @@
 function onInitGallery(e){
     e.preventDefault();
     // ToDo: Initialize list view
-    
+    var itemWidth = $(window).width()/3;
+	
+	 //$('#gallery-grid').attr('width', $(window).width());
      $('#gallery-grid').isotope({
-	  // options
-		  itemSelector: '.gallery-item',
-		  layoutMode: 'packery'
-	});
+		itemSelector: '.gallery-item',
+		layoutMode: 'packery',
+		  masonry: {
+        	columnWidth: itemWidth
+      		}
+		});
+	
 	}
 
 
 function onShowGallery(e) {
 	e.preventDefault();
-	var grid = $('#gallery-grid');
+	var grid = $('#gallery-grid'), isotope = grid.data('isotope');
+	var itemWidth = $(window).width()/3;
+	itemWidth -= 2;  // account for the borders
+	
 	//Clear out previous content
 	grid.empty();
 	
 	var photoArray = APP.models.gallery.photosDS.data();
 	
 	for (var i=0; i< photoArray.length; i++) {
-		grid.append('<div class="gallery-item"> <img id=" + photoArray[i].photoId + '" src="' + photoArray[i].thumb + '"/> </div>');
+		grid.append('<div class="gallery-item" style="height: auto; width='+ itemWidth +  'px;" > <img  id="' 
+					+ photoArray[i].photoId + '" src="' + photoArray[i].thumbnailUrl + '"/> </div>');
 	}
 	
+	isotope.layout();
 }
 
