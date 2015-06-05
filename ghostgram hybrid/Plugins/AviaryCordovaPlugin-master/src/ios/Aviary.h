0f24c30e9bd1438e59b56479c36cfcf14a15baf6
don bradford <donbrad@hotmail.com>
2015-06-05 01:18:13 +0000

    Changes made by don bradford (donbrad@hotmail.com).

---
+++ ghostgram hybrid/Plugins/AviaryCordovaPlugin-master/src/ios/Aviary.h
@@ -0,0 +1,24 @@
+// AviaryGap - v.1.0.0
+// (c) 2013 Ryan Vanderpol, me@ryanvanderpol.com, MIT Licensed.
+// AviaryPlugin.h may be freely distributed under the MIT license.
+//
+//  AviaryPlugin.h
+//
+
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+#import "AVYPhotoEditorController.h"
+
+@interface Aviary : CDVPlugin <AVYPhotoEditorControllerDelegate> {
+    AVYPhotoEditorController* aviary;
+}
+
+@property (nonatomic, retain) AVYPhotoEditorController *aviary;
+@property (nonatomic, retain) NSString* pluginCallbackId;
+@property (nonatomic, retain) NSNumber* quality;
+@property (nonatomic, retain) NSString* originalImageURI;
+
+- (void) prepareForShow:(CDVInvokedUrlCommand*)command;
+- (void) show:(CDVInvokedUrlCommand*)command;
+
+@end
