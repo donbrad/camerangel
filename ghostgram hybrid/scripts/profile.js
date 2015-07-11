f53b0616733ed3285824d0cb02ccce562feac1b9
Don Bradford <donbrad@hotmail.com>
2015-07-11 09:38:51 -0700

    working on profile photo

--- ghostgram hybrid/scripts/profile.js
+++ ghostgram hybrid/scripts/profile.js
@@ -8,7 +8,9 @@ function closeChooseGhost() {
 
 
 // Select new ghost icon
-function whichGhost(e){
+function whichGhost(e) {
+    if (e.preventDefault !== undefined)
+        e.preventDefault();
     var selection = e.target[0].id;
     var selectionPath = "images/" + selection + ".svg";
     var currentAlias = APP.models.profile.currentUser.aliasPhoto;
@@ -22,7 +24,8 @@ function whichGhost(e){
 }
 
 
-// Todo - wire save profile, may not need w/ profile sync
+
+
 function saveEditProfile() {
     mobileNotify("Your profile was updated")
 }
@@ -35,7 +38,9 @@ function closeNewPass() {
 }
 
 function validNewPass(e) {
-    e.preventDefault();
+    if (e.preventDefault !== undefined)
+        e.preventDefault();
+
     var pass1 = $("#newPassword1").val();
     var pass2 = $("#newPassword2").val();
 
@@ -61,4 +66,66 @@ function validNewPass(e) {
         }
 
     }
+}
+
+function profilePhotoScaleSuccess (data) {
+    var scaledImageUrl = data.imageData;
+
+}
+
+function updateProfilePhoto (e) {
+    if (e.preventDefault !== undefined)
+        e.preventDefault();
+
+    var pictureSource = navigator.camera.PictureSourceType;   // picture source
+    var destinationType = navigator.camera.DestinationType; // sets the format of returned value
+    // Android storage is seriously different -- multiple photo directories with different permissions.
+    // So need to get a data url in our space rather an direct link to the image in current storage
+    var options = {
+        sourceType: pictureSource.SAVEDPHOTOALBUM,
+        destinationType: destinationType.DATA_URL
+    }
+    if (device.platform === 'iOS') {
+        options = {
+            sourceType: pictureSource.SAVEDPHOTOALBUM,
+            destinationType: destinationType.FILE_URL
+        }
+    }
+    navigator.camera.getPicture(
+        function (imageData) {
+            var photouuid = uuid.v4();
+            var imageUrl = imageData;
+            var displayUrl = imageData;
+            var scaledImageUrl  = '';
+
+            if (device.platform === 'iOS') {
+                imageUrl = imageData.replace('file://', '');
+
+            }	else {
+                displayUrl = "data:image/jpg;base64," + imageData;
+            }
+            // convert uuid into valid file name;
+            var filename = photouuid.replace(/-/g,'');
+
+           /* APP.models.gallery.currentPhoto.photoId = photouuid;
+            APP.models.gallery.currentPhoto.filename = filename;
+            APP.models.gallery.currentPhoto.imageUrl = imageUrl;
+            */
+
+            //resize image to 1200 pixels high
+            /*		   window.imageResizer.resizeImage(resizeSuccess, resizeFailure,  imageUrl, 0, 1200, {
+             quality: 75, storeImage: 1, photoAlbum: 0, filename: "photo_"+filename+'.jpg' }); */
+
+            if (device.platform === 'iOS') {
+                window.imageResizer.resizeImage(profilePhotoScaleSuccess, resizeFailure,  imageUrl, 0, 140, {
+                    storeImage: false, pixelDensity: true, quality: 95 });
+            } else {
+                window.imageResizer.resizeImage(profilePhotoScaleSuccess, resizeFailure,  imageUrl, 0, 140, {
+                    imageDataType: ImageResizer.IMAGE_DATA_TYPE_BASE64, storeImage: false, pixelDensity: true, quality: 95 });
+            }
+        },
+        function (error) {
+            mobileNotify("Camera error " + error);
+        }, options
+    );
 }
