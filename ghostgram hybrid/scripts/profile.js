ce377368616456df91afbe31b7e65467382eedd9
Don Bradford <donbrad@hotmail.com>
2015-07-13 13:31:10 -0700

    adding smart cropping for user and contact profile images

--- ghostgram hybrid/scripts/profile.js
+++ ghostgram hybrid/scripts/profile.js
@@ -66,7 +66,7 @@ function validNewPass(e) {
 }
 
 function profilePhotoScaleSuccess (data) {
-    var scaledImageUrl = data.imageData;
+    var scaledImageUrl = "data:image/jpg;base64," + data.imageData;
     $('#photoEditImage').attr('src', scaledImageUrl);
     APP.kendo.navigate('#editPhoto?source=profile')
 }
