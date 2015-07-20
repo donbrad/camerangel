b7ebd6fd948639ecdef70fc7538c6d62d38c8e75
Don Bradford <donbrad@hotmail.com>
2015-07-20 13:22:09 -0700

    updating places schema in code

--- ghostgram hybrid/scripts/places.js
+++ ghostgram hybrid/scripts/places.js
@@ -23,7 +23,8 @@ function onInitPlaces(e) {
 					uuid: uuid.v4(),
 					category: 'Place',
 					placeId: e.dataItem.place_id,
-					name: e.dataItem.name,
+					name: e.dataItem.name,   // Name created / edited by user
+					venueName: e.dataItem.name,   // Name from googlePlace or factual
 					streetNumber: address.streetNumber,
 					street: address.street,
 					city: address.city,
@@ -180,7 +181,7 @@ function onShowEditPlace (e) {
 			APP.models.places.currentPlace.set('name', $('#editPlaceName').val());
 		});
 
-	if (APP.models.places.currentPlace.category === 'Street Address') {
+	if (APP.models.places.currentPlace.category === 'Location') {
 		$('#editPlaceAddress').show().next('br').remove();
 		$('#editPlaceAddress')
 			.val(APP.models.places.currentPlace.streetNumber)
@@ -420,9 +421,10 @@ function onLocateMe(e) {
 
 							var newPlace = APP.models.places.placesDS.add({
 								uuid: uuid.v4(),
-								category: 'Street Address',
+								category: 'Location',   // valid categories are: Place and Location
 								placeId: '',
 								name: '',
+								venueName: '',
 								streetNumber: address.streetNumber,
 								street: address.street,
 								city: address.city,
@@ -435,8 +437,8 @@ function onLocateMe(e) {
 								lng: position.coords.longitude,
 								publicName: '',
 								alias: '',
-								visible: true,
-								privacy: true,
+								isVisible: true,
+								isPrivate: true,
 								autoCheckIn: false
 							});
 
@@ -579,8 +581,8 @@ function addPlaceAdd(e) {
 	place.set('googleId', APP.models.places.currentGeoPlace.place_id);
 	place.set('name', $('#addPlaceName').val());
 	place.set('address', $('#addPlaceAddress').val());
-	place.set('privacy', $('#addPlacePrivacy').val());
-	place.set('visible', $('#addPlaceVisible').val());
+	place.set('isPrivate', $('#addPlacePrivacy').val());
+	place.set('isVisible', $('#addPlaceVisible').val());
 	place.set('lat',  APP.models.places.currentGeoPlace.lat);
 	place.set('lng', APP.models.places.currentGeoPlace.lng);
 	
