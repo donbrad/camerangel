613cef37b851bd93db009df0dd9c12478062da76
Don Bradford <donbrad@hotmail.com>
2015-07-20 19:19:05 -0700

    integrated ghostMail test - both encrypted and cleartext in email

--- ghostgram hybrid/scripts/contacts.js
+++ ghostgram hybrid/scripts/contacts.js
@@ -984,7 +984,7 @@ function sendGhostEmail(e) {
         cordova.plugins.email.open({
             to:          [email],
             subject:     'ghostEmail',
-            body:        '<h2>ghostEmail From ' + thisUser+ '</h2> <p>'+ content +'</p> <p>'+ encryptContent + '</p>',
+            body:        '<h2>ghostEmail From ' + thisUser+ '</h2> <p> !!Test - clear text included !!</p><p>'+ content +'</p> <p>'+ encryptContent.cipher + '</p>',
             isHtml:      true
         }, function (msg) {
             // navigator.notification.alert(JSON.stringify(msg), null, 'EmailComposer callback', 'Close');
