bd2dc4810a5888e82cb45c5aeab47b3ca3db9bc2
Tucker Connelly <web@tuckerconnelly.com>
2015-07-24 19:33:26 +0200

    Make the archive pretty

--- ghostgram hybrid/scripts/archiveView.js
+++ ghostgram hybrid/scripts/archiveView.js
@@ -4,40 +4,155 @@ var archiveView = {
 
 	init: function () {
 		if (archive.dataSource.total() === 0 && 1) {
+			// chat
 			archive.add({
-				relID: 1,
-				value: 'Testing yo',
-				date: new Date(),
-				internalTags: 'Don',
-				userTags: 'blue, red',
-				category: 'chat'
+				message: {
+					channelId: 'asdf',
+					channelName: 'myplacez design',
 
+					sender: {
+						id: 'asdf',
+						name: 'Don Bradford',
+						image: 'http://files.parsetfss.com/b7762404-2bdf-4947-a141-9fdd3b9daf41/tfss-c1b74c69-deea-43a4-b36a-685f95b76f90-61ebcea2-9085-4a3d-99f2-58e7cfe04eb3.png'
+					},
+
+					placeId: 'asdf',
+					placeName: 'Home',
+					address: '221 Davis St., Greenfield, MA',
+
+					eventId: 'asdf',
+					eventName: 'Work at Don\'s',
+
+					text: 'Some chat about myplacez',
+					date: new Date()
+				},
+				type: 'Chat',
+				tags: '',
+				object: {}
 			});
 
+			// events
 			archive.add({
-				relID: 2,
-				value: 'More tests!',
-				date: new Date(),
-				internalTags: 'Don',
-				userTags: 'green',
-				category: 'chat'
+				message: {
+					channelId: 'asdf',
+					channelName: 'myplacez design',
+
+					sender: {
+						id: 'asdf',
+						name: 'Don Bradford',
+						image: 'http://files.parsetfss.com/b7762404-2bdf-4947-a141-9fdd3b9daf41/tfss-c1b74c69-deea-43a4-b36a-685f95b76f90-61ebcea2-9085-4a3d-99f2-58e7cfe04eb3.png'
+					},
+
+					placeId: 'asdf',
+					placeName: 'Home',
+					address: '221 Davis St., Greenfield, MA',
+
+					eventId: 'asdf',
+					eventName: 'Work at Don\'s',
+
+					text: 'Awesome concert next weekend!',
+					date: new Date()
+				},
+				type: 'Events',
+				tags: '',
+				object: {
+					name: 'Margot and the Nukes',
+
+					placeId: 'asdf',
+					placeName: 'Iron Horse',
+					address: '24 Main St., Northampton, MA',
+					
+					start: new Date(),
+					end: new Date()
+				}
+			});
+			
+			// urls
+			archive.add({
+				message: {
+					channelId: 'asdf',
+					channelName: 'myplacez design',
+
+					sender: {
+						id: 'asdf',
+						name: 'Don Bradford',
+						image: 'http://files.parsetfss.com/b7762404-2bdf-4947-a141-9fdd3b9daf41/tfss-c1b74c69-deea-43a4-b36a-685f95b76f90-61ebcea2-9085-4a3d-99f2-58e7cfe04eb3.png'
+					},
+
+					placeId: 'asdf',
+					placeName: 'Home',
+					address: '221 Davis St., Greenfield, MA',
+
+					eventId: 'asdf',
+					eventName: 'Work at Don\'s',
+
+					text: 'Check out this refactoring thing',
+					date: new Date()
+				},
+				type: 'Links',
+				tags: '',
+				object: {
+					title: 'Refactoring',
+					url: 'http://www.refactoring.com/'
+				}
+			});
+			
+			// photos
+			archive.add({
+				message: {
+					channelId: 'asdf',
+					channelName: 'myplacez design',
+
+					sender: {
+						id: 'asdf',
+						name: 'Don Bradford',
+						image: 'http://files.parsetfss.com/b7762404-2bdf-4947-a141-9fdd3b9daf41/tfss-c1b74c69-deea-43a4-b36a-685f95b76f90-61ebcea2-9085-4a3d-99f2-58e7cfe04eb3.png'
+					},
+
+					placeId: 'asdf',
+					placeName: 'Home',
+					address: '221 Davis St., Greenfield, MA',
+
+					eventId: 'asdf',
+					eventName: 'Work at Don\'s',
+
+					text: 'Check out this place photo',
+					date: new Date()
+				},
+				type: 'Photos',
+				tags: '',
+				object: {
+					url: 'http://www.craterlakenational.com/image/crater_lake_large1.jpg',
+
+					placeName: 'Crater Lake National Park',
+					address: 'OR',
+
+					date: new Date()
+				}
 			});
 		}
 
 		archiveView.checkIfEmpty();
+
+		// Binding this manually because data-role="button" messes up the styles
+		$('#archive-list').on('click', '.object', archiveView.openObject);
 
 		//archive.search('green since jan 1st 14');
 	},
 
 	checkIfEmpty: function () {
 		if (archive.dataSource.total() === 0) {
-			$('#archive .nothing-found').show();
-			$('#archive main > *:not(.nothing-found)').hide();
+			// If the total still equals 0 after remove the filters
+			archive.dataSource.filter({});
+			if (archive.dataSource.total() === 0) {
+				$('#archive .nothing-found').show();
+				$('#archive main > *:not(.nothing-found)').hide();
+			}
 		}
 	},
 
 	search: function () {
-		if (archives.search($('#search-archives input').val()) === false) {
+		if (archive.search($('#search-archives input').val()) === false) {
 			// Show nothing found
 		};
 	},
@@ -48,5 +163,12 @@ var archiveView = {
 		);
 
 		archiveView.checkIfEmpty();
+	},
+
+	openObject: function (e) {
+		var $object = $(e.currentTarget);
+		if ($object.hasClass('link')) {
+			window.open($object.data('url'), '_system');;
+		}
 	}
 };
