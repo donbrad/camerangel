eaa4a60a3406d3eee3d0431118e5258c408b7056
Don Bradford <donbrad@hotmail.com>
2015-09-08 10:22:01 -0700

    working on click bug in channelsView -- can't really use click handler with touch...

--- ghostgram hybrid/scripts/views/channelViews.js
+++ ghostgram hybrid/scripts/views/channelViews.js
@@ -40,7 +40,7 @@ var channelsView = {
             enableSwipe: true,
             tap: function (e) {
                 //var selector = e.target[0].parentElement;
-                var selection = e.sender.events.currentTarget;
+                var selector = e.sender.events.currentTarget;
                 if($(selector).hasClass("chat-mainBox") === true || e.target[0].className === "chat-mainBox"){
                     var channelUrl = "#channel?channel=" + e.dataItem.channelId;
                     APP.kendo.navigate(channelUrl);
