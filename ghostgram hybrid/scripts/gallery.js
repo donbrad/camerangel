8bafef4958f5ecea3faaa96b0b53e5d730f9c903
Don Bradford <donbrad@hotmail.com>
2015-09-10 12:44:49 -0700

    refactored gallery into galleryView per jordan's request

--- ghostgram hybrid/scripts/gallery.js
+++ ghostgram hybrid/scripts/gallery.js
@@ -25,83 +25,7 @@ var getSentinelHeight = function () {
 	return combinedHeight/16+'rem';
 };
 
-function onInitGallery(e){
-	archiveView.init();
 
-	var setSentinelHeight = function () {
-		$('#search-archives').height(getSentinelHeight());
-	};
-/*
-	archiveView.sentinel.addListener('add', setSentinelHeight);
-	archiveView.sentinel.addListener('remove', setSentinelHeight);
-	setSentinelHeight();
-*/
-
-   if (e !== undefined && e.preventDefault !== undefined){
-		e.preventDefault();
-	}
-    // ToDo: Initialize list view
-    var itemWidth = $(window).width()/4;
-	photoModel.rotationAngle = 0;
-	photoModel.optionsHidden = true;
-	photoModel.previewSize = "33%";
-	photoModel.optionsShown = true;
-
-
-	$("#gallerySearch").keyup(function() {
-  		var query = $("#gallerySearch").val();
-		if (query.length > 0) {
-			
-		}
-		
-	});
-
-	// Kendo web bug is firing infinitely
-	if (window.navigator.simulator === false) {
-		$("#gallerySearch").on("focus", function(){
-			$(".gallerySearchOptions").velocity("slideDown", {duration: 300});
-			//$("#gallerySearch").unbind("focus");
-			console.log("focus");
-		});
-
-		$("#gallerySearch").on("blur", function(){
-			$(".gallerySearchOptions").velocity("slideUp", {duration: 300});
-			//$("#gallerySearch").unbind("blur");
-			console.log("blur");
-		});
-	} else {
-		$(".gallerySearchOptions").velocity("slideDown", {duration: 300});
-	}
-	// hide archive options
-	$(".gallerySearchOptions, #galleryPhotoDisplayOpts").css("display", "none");
-	
-
-	var scroller = e.view.scroller;
-	//scroller.scrollTo(0,-44);
-	/*
-	var currentPos = 0;
-	scroller.bind("scrollingDown");
-	scroller.bind("scrollingUp");
-	scroller.bind("scroll", function(){
-		console.log("currentPos: " + currentPos + " scrollTop: " + scroller.scrollTop);
-		if(currentPos > scroller.scrollTop && scroller.scrollTop > 0){
-			console.log("plus");
-			currentPos = scroller.scrollTop;
-			scroller.trigger("scrollingDown", resetNavUI());
-			//scroller.unbind("scrollingDown");
-		} else if (scroller.scrollTop > currentPos){
-			console.log("minus");
-			currentPos = scroller.scrollTop;
-			scroller.trigger("scrollingUp", shrinkNavUI());
-			//scroller.unbind("scrollingUp");
-		} else {
-			console.log("none");
-		}
-		console.log(currentPos);
-	});    
-	
-*/
-}
 
 function resetNavUI(e){
 	$(".km-navbar").removeClass("home-smallHeader");	
@@ -112,89 +36,11 @@ function resetNavUI(e){
 function shrinkNavUI() {
 	$(".km-navbar").addClass("home-smallHeader");	
 	$(".user-status, .user-settings").velocity("fadeOut", {duration: 150});
-	console.log("shrinkNavUI");
-}
-
-function galleryOptionsToggle (e) {
-	if (e !== undefined && e.preventDefault !== undefined){
-		e.preventDefault();
-	}
-	
-
-	if (photoModel.optionsShown) {
-		$("#galleryToggle").velocity("fadeOut",{duration: 150});
-		$('.gallerySearchOptions').velocity("slideDown",{duration: 300});
-		$("#galleryZoomSelect > li:first-child").velocity("fadeIn", {duration: 300});
-		photoModel.optionsShown = false;
-		$("#gallerySearch").focus();
-	} else {
-		$('.gallerySearchOptions').velocity("slideUp",{duration: 300});
-		$("#galleryToggle").velocity("fadeIn",{delay: 150, duration: 150});
-		$("#galleryZoomSelect > li:first-child").velocity("fadeOut", {duration: 300});
-		//$('#gallerySearchOptions').removeClass('hidden');
-		photoModel.optionsShown = true;
-		
-	}
-	
+	//console.log("shrinkNavUI");
 }
 
 
-function selectGallerySearchTool(e) {
-	if (e !== undefined && e.preventDefault !== undefined)
-		e.preventDefault();
-	var index = this.current().index();
-	
-	switch (index) {
-			
-		case 0: // Search
-			$("#gallerySearch").attr("placeholder", "Search all");
-			break;
-			
-		case 1: // Contacts
-			$("#gallerySearch").attr("placeholder", "Search contacts");
-			break;
-			
-		case 2: // Chats
-			$("#gallerySearch").attr("placeholder", "Search chats");
-			break;
-			
-		case 3: // Calendar
-			$("#gallerySearch").attr("placeholder", "Search dates");
-			break;
-			
-		case 4: // Places
-			$("#gallerySearch").attr("placeholder", "Search places");
-			break;
-	}
-	
-}
 
-function selectGalleryZoom(e) {
-		if (e !== undefined && e.preventDefault !== undefined)
-		e.preventDefault();
-
-	var index = this.current().index();
-	switch (index) {
-		case 0:
-			photoModel.previewSize = "33%";
-			$("#gallery-listview li").css("width","33%");
-			$("#gallery-listview li").css("padding-bottom","33%");
-			break;
-		case 1:
-			photoModel.previewSize = "50%";
-			$("#gallery-listview li").css("width","50%");
-			$("#gallery-listview li").css("padding-bottom","50%");
-			break;
-		case 2:
-			photoModel.previewSize = "100%";
-			$("#gallery-listview li").css("width","100%");
-			$("#gallery-listview li").css("padding-bottom","100%");
-			break;
-			
-	}
-	
-	
-}
 
 function photoEditCrop(e) {
 	if (e !== undefined && e.preventDefault !== undefined)
@@ -288,86 +134,7 @@ function onHidePhotoView(e) {
 }
 
 
-function onShowGallery(e) {
-	e.preventDefault();
-	
-	photoModel.chatPhoto = false;
-	if (e.view.params.action !== undefined && e.view.params.action === 'chat') {
-		photoModel.chatPhoto = true;
-		mobileNotify("Please select an image to send...")
-	}
-	photoModel.rotationAngle = 0;
-	
 
-	$("#gallery-listview li").css("width",photoModel.previewSize);
-	$("#gallery-listview li").css("padding-bottom",photoModel.previewSize);
-
-
-	switch(photoModel.previewSize) {
-		case "33%" :
-			//setButtonGroupIndex("#gallerySearchToolSelect", 0);
-			break;
-
-		case "50%" :
-			//setButtonGroupIndex("#gallerySearchToolSelect", 1);
-			break;
-
-		case "100%" :
-			//setButtonGroupIndex("#gallerySearchToolSelect", 2);
-			break;
-	}
-
-	// Added until gallery/archive are fully merged
-	checkEmptyUIState("#archive-listview", "#archiveBox");
-}
-
-
-function gallerySelectCategory(e){
-	var index = this.current().index();
-	switch (index) {
-		case 0:
-			$(".gallerySearchOptions").velocity("slideDown");
-			$("#galleryPhotoDisplayOpts").velocity("slideUp");
-
-			$('#archive-listview').removeClass('hidden');
-			$("#gallery-listview").addClass('hidden');
-
-			break;
-
-		case 1:
-			$(".gallerySearchOptions").velocity("slideUp");
-			$("#galleryPhotoDisplayOpts").velocity("slideDown");
-
-			$('#archive-listview').addClass('hidden');
-			$("#gallery-listview").removeClass("hidden");
-			break;
-	}
-	$("#gallerySearch").attr("placeholder", "Search All");
-
-
-
-} 
-
-function galleryClick(e) {
-	if (e !== undefined && e.preventDefault !== undefined) {
-		e.preventDefault();
-	}
-
-	var photoId = e.dataItem.id, photoUrl = e.dataItem.imageUrl;
-	photoModel.currentPhotoModel = getPhotoModel(photoId);
-	$('#photoViewImage').attr('src', photoUrl);
-	$('#photoTagImage').attr('src', photoUrl);
-	$('#photoEditImage').attr('src', photoUrl);
-
-	if (photoModel.chatPhoto) {
-		showChatImagePreview(photoUrl);
-		APP.kendo.navigate('#:back');
-
-	} else {
-		APP.kendo.navigate('#photoView');
-	}
-
-}
 
 
 function getPhotoModel(photoId) {
@@ -390,8 +157,6 @@ function photoDelete (e) {
 	e.preventDefault();
 	var photo = photoModel.currentPhotoModel;
 	// Todo:  Add confirmation prior to photo delete
-	
-
 	
 	mobileNotify("Deleted current photo");
 	
