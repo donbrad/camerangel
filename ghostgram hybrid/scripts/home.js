0fc91715653a96ad8bccf1432c23bb4e2aff8590
Tucker Connelly <web@tuckerconnelly.com>
2015-09-15 13:39:25 -0700

    Places work

--- ghostgram hybrid/scripts/home.js
+++ ghostgram hybrid/scripts/home.js
@@ -5,17 +5,17 @@
 var homeView = {
 	openLocateMeModal: function () {
 		$('#modalview-locate-me').data('kendoMobileModalView').open();
-	
+
 		navigator.geolocation.getCurrentPosition( function (position) {
 			var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 			var places = APP.map.googlePlaces;
 			var nearbyResults = new kendo.data.DataSource();
 
-			var locations = placesView.matchLocationToUserPlace(position.coords.latitude, position.coords.longitude);
+			var userPlaces = placesView.matchLocationToUserPlace(position.coords.latitude, position.coords.longitude);
 
-			if (locations.length > 0) {
-				placesView.checkInTo(locations[0]);
-			}
+			userPlaces.forEach( function (userPlace ) {
+				nearbyResults.add(userPlace);
+			});
 
 			places.nearbySearch({
 				location: latlng,
@@ -65,7 +65,41 @@ var homeView = {
 				}
 
 				placesResults.forEach( function (placeResult) {
-					nearbyResults.add(placeResult);
+					var alreadyAUserPlace = false;
+					userPlaces.forEach( function (userPlace) {
+						if (userPlace.googleId === placeResult.place_id) {
+							alreadyAUserPlace = true;
+							return;
+						}
+					});
+
+					nearbyResults.add({
+						uuid: '',
+						category: 'Street Address',   // valid categories are: Place and Location
+						placeId: '',
+						name: placeResult.name,
+						venueName: placeResult.name,
+						streetNumber: '',
+						street: '',
+						city: '',
+						state: '',
+						zip: '',
+						country: '',
+						googleId: placeResult.place_id,
+						factualId: '',
+						lat: placeResult.geometry.location.G,
+						lng: placeResult.geometry.location.K,
+						publicName: '',
+						alias: '',
+						isVisible: true,
+						isPrivate: true,
+						autoCheckIn: false,
+						vicinity: placeResult.vicinity
+					});
+
+					if (alreadyAUserPlace === false) {
+						nearbyResults.add(placeResult);
+					}
 				});
 
 				$('#nearby-results-list').data('kendoMobileListView').setDataSource(nearbyResults);
@@ -81,13 +115,41 @@ var homeView = {
 
 	checkInToPlace: function (e) {
 		var item = e.item.children('div').first().data('item');
-		
-		$('#checked-in-place > span').html(item.name);
-		$('#checked-in-place').show(200);
-		$('#modalview-locate-me').data('kendoMobileModalView').close();
 
-		userModel.currentUser.set('currentPlace', item.name);
-		userModel.currentUser.set('currentPlaceUUID', item.uuid);
+		var finishCheckingIn = function (item) {
+			$('#checked-in-place > span').html(item.name);
+			$('#checked-in-place').show(200);
+			$('#modalview-locate-me').data('kendoMobileModalView').close();
+
+			APP.models.places.placesDS.add(item);
+
+			userModel.currentUser.set('currentPlace', item.name);
+			userModel.currentUser.set('currentPlaceUUID', item.uuid);
+		};
+
+		// If the item has a uuid it means we've already added it,
+		// or it's a street address so we don't have to find the
+		// address.
+		if (item.uuid !== '') {
+			finishCheckingIn(item);
+			return;
+		}
+
+		var latlng = new google.maps.LatLng(item.lat, item.lng);
+
+		// Otherwise it's a new place from Google so we find the address
+		APP.map.geocoder.geocode({ 'latLng': latlng }, function (geoResults, geoStatus) {
+			if (geoStatus !== google.maps.GeocoderStatus.OK) {
+				navigator.notification.alert('Something went wrong with the Google geocoding service.');
+				return;
+			}
+			if (geoResults.length === 0 || geoResults[0].types[0] !== 'street_address') {
+				navigator.notification.alert('We couldn\'t match your position to a street address.');
+				return;
+			}
+
+			var address = placesView.getAddressFromComponents(geoResults[0].address_components);
+		});
 	},
 
 	checkOutOfPlace: function () {
