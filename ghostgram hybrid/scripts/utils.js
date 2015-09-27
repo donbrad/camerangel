49f38fea51937597710226c295f8a3df1737e73b
Don Bradford <donbrad@hotmail.com>
2015-09-27 06:33:28 -0700

    adding smart truncate

--- ghostgram hybrid/scripts/utils.js
+++ ghostgram hybrid/scripts/utils.js
@@ -12,7 +12,7 @@ String.prototype.smartTruncate =
 		var toLong = this.length>n,
 			s_ = toLong ? this.substr(0,n-1) : this;
 		s_ = useWordBoundary && toLong ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
-		return  toLong ? s_ + '&hellip;' : s_;
+		return  toLong ? s_ + '...' : s_;
 	};
 
 function updateParseObject(objectName, idField, idFieldValue, newField, newFieldValue) {
