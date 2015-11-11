dac2b7f8b9f30783277aa9460394053db9aef224
Don Bradford <donbrad@hotmail.com>
2015-11-11 09:13:38 -0800

    unifying naming of ghostgrams gallery to Memories Gallery

--- ghostgram hybrid/scripts/views/galleryView.js
+++ ghostgram hybrid/scripts/views/galleryView.js
@@ -555,6 +555,7 @@ var galleryPicker = {
         var photo = e.dataItem;
         galleryPicker.photo = photo;
 
+        gallerPicker.closeModal();
         if (galleryPicker._callback !== null) {
             galleryPicker._callback(photo);
         }
