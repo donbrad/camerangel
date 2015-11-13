103556a828ccf739ba7c98eb9c69b6767e480fb6
Don Bradford <donbrad@hotmail.com>
2015-11-13 14:35:45 -0800

    adding profileView and updating profile views to facilitate integrtion of new profile photo update / edit

---
+++ ghostgram hybrid/scripts/views/profileView.js
@@ -0,0 +1,60 @@
+/**
+ * Created by donbrad on 9/29/15.
+ * profileView.js -- all view management for profile edit and status
+ */
+
+var profileView = {
+    onInit: function (e) {
+        _preventDefault(e);
+
+
+    },
+
+    onShow: function (e) {
+        _preventDefault(e);
+        if (userModel.currentUser.emailVerified){
+            $("#verified-email").removeClass("hidden");
+        }
+
+        if(userModel.currentUser.phoneVerified){
+            $("#verified-phone").removeClass("hidden");
+        }
+
+        // format phone number
+        ux.showFormatedPhone();
+    }
+
+};
+
+var profileEditView = {
+
+    _activeProfile : new kendo.data.ObservableObject(),
+
+    onInit: function (e) {
+        _preventDefault(e);
+
+    },
+
+    onShow: function (e) {
+        _preventDefault(e);
+
+        profileEditView._activeProfile.set('name', userModel.currentUser.get('name'));
+        profileEditView._activeProfile.set('alias', userModel.currentUser.get('alias'));
+        profileEditView._activeProfile.set('email', userModel.currentUser.get('email'));
+        profileEditView._activeProfile.set('photo', userModel.currentUser.get('photo'));
+
+    },
+
+    doSave : function (e) {
+        _preventDefault(e);
+
+        mobileNotify("Updating your profile...");
+
+        userModel.currentUser.set('name', profileEditView._activeProfile.get('name'));
+        userModel.currentUser.set('alias', profileEditView._activeProfile.get('alias'));
+        userModel.currentUser.set('email', profileEditView._activeProfile.get('email'));
+        userModel.currentUser.set('photo', profileEditView._activeProfile.get('photo'));
+
+        APP.kendo.navigate('#profile');
+    }
+};
