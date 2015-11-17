64fa1aadd2f6d076efc3504b3820c96f418ae9a4
Don Bradford <donbrad@hotmail.com>
2015-11-17 08:33:58 -0800

    integrated new server push notification code for android and ios

--- ghostgram hybrid/scripts/serverPush.js
+++ ghostgram hybrid/scripts/serverPush.js
@@ -11,20 +11,19 @@ var serverPush = {
     _googleSenderId : "962500978306",   // contact donbrad before changing...
     _regId : null,
     _channelsProvisioned : false,
+    plugin : window.plugins.pushNotification,
 
     init : function () {
+        serverPush.plugin = window.plugins.pushNotification;
 
-        /*serverPush.plugin = window.PushNotification.init({
-            "android": {"senderID": serverPush._googleSenderId},
-            "ios": {"alert": "true", "badge": "true", "sound": "true"},
-            "windows": {}
-        });
+        if (device.platform == 'android' || device.platform == 'Android' || device.platform == 'amazon-fireos' ) {
+            serverPush.plugin.register(serverPush.onSuccess, serverPush.onError(),
+                {senderID:serverPush._googleSenderId, ecb: serverPush.onNotificationECM});		// required!
+        } else if (device.platform == 'iOS') {
+            serverPush.plugin.register(serverPush.onRegistration, errorHandler,
+                {"badge":"true","sound":"true","alert":"true","ecb": serverPush.onNotificationAPN});	// required!
+        }
 
-        serverPush.plugin.on('registration', this.onRegistration);
-
-        serverPush.plugin.on('notification', this.onNotification);
-
-        serverPush.plugin.on('error', this.onError);*/
 
     },
 
@@ -39,14 +38,82 @@ var serverPush = {
 
     },
 
-    onNotification : function (data) {
-        mobileNotify("Push: " +  data.title + " : " + data.message);
-        // data.message,
-        // data.title,
-        // data.count,
-        // data.sound,
-        // data.image,
-        // data.additionalData
+
+    onNotificationAPN : function (e) {
+
+        if (e.alert) {
+            navigator.notification.alert(e.alert);
+        }
+
+        if (e.sound) {
+            // playing a sound also requires the org.apache.cordova.media plugin
+            var snd = new Media(e.sound);
+            snd.play();
+        }
+
+        if (e.badge) {
+            pushNotification.setApplicationIconBadgeNumber(serverPush.onSuccess, e.badge);
+        }
+    },
+
+    onNotificationECM : function (data) {
+
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    mobileNotify("Notification: Android regID = " + e.regid);
+                    serverPush._regId =  e.regid;
+
+                    deviceModel.setAppState('isDeviceRegistered', true);
+
+                    deviceModel.isPushProvisioned();
+                }
+                break;
+
+            case 'message':
+                // if this flag is set, this notification happened while we were in the foreground.
+                // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                if (e.foreground)
+                {
+                    // on Android soundname is outside the payload.
+                    // On Amazon FireOS all custom attributes are contained within payload
+                    var soundfile = e.soundname || e.payload.sound;
+                    // if the notification contains a soundname, play it.
+                    // playing a sound also requires the org.apache.cordova.media plugin
+                    var my_media = new Media("/android_asset/www/"+ soundfile);
+
+                    my_media.play();
+                }
+                else
+                {	// otherwise we were launched because the user touched a notification in the notification tray.
+                    if (e.coldstart)
+                        mobileNotify('Notification : Cold Start');
+
+                }
+
+                navigator.notification.alert(e.payload.message);
+
+                break;
+
+            case 'error':
+
+                mobileNotify("Notification Error : " + e.msg);
+                break;
+
+            default:
+               mobileNotify('Notification Unknown: an event was received and we do not know what it is');
+                break;
+        }
+
+    },
+
+    onSuccess : function (e) {
+        // e.message
+        mobileNotify("Server push : " + e.message);
     },
 
     onError : function (e) {
@@ -56,7 +123,7 @@ var serverPush = {
 
     provisionDataChannels : function () {
 
- /*       if (!serverPush._channelsProvisioned) {
+        if (!serverPush._channelsProvisioned) {
             var type = 'apns';
 
             if (device.platform === "Android") {
@@ -85,7 +152,7 @@ var serverPush = {
             serverPush._channelsProvisioned = true;
 
         }
-*/    },
+    },
 
     _success : function (data) {
         mobileNotify("Data channel server push enabled!");
