3bdb1cfb691e5845220f93db4c6c9243c03249f9
Don Bradford <donbrad@hotmail.com>
2015-12-11 08:14:55 -0800

    unifying photo display around thumbnailUrl  only use imageUrl for high resolution zoomable views...

--- ghostgram hybrid/scripts/views/channelViews.js
+++ ghostgram hybrid/scripts/views/channelViews.js
@@ -1367,7 +1367,7 @@ var channelView = {
         // User actually clicked on the photo so show the open the photo viewer
         if (target.hasClass('chat-message-photo')) {
         	// Open this img full screen
-            var photoObj = data.photo;
+            var photoObj = message.data.photo;
           /*  var photoUrl = message.data.photo.photo;
             $('#modalPhotoViewImage').attr('src', photoUrl);*/
             modalPhotoView.openModal(photoObj);
