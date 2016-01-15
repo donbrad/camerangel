1d4818f405060b41aff04881018ae16323af9cbf
Don Bradford <donbrad@hotmail.com>
2016-01-15 15:52:07 -0800

    adding placeName to smartEvent button

--- ghostgram hybrid/scripts/views/channelViews.js
+++ ghostgram hybrid/scripts/views/channelViews.js
@@ -1786,7 +1786,7 @@ var channelView = {
         var date = smartObject.date.toLocaleString();
         var dateStr = moment(date).format('dddd, MMMM Do h:mm a');
         var objectUrl = '<a data-role="button" class="btnSmart" data-objectid="'+ objectId + '" id="chatobject_' + objectId + '" data-click="channelView.onObjectClick"> <img src="images/smart-event-light.svg" class="icon-smart" /> '  + ' ' + smartObject.title + "</br> " +
-            dateStr +  '</a>';
+            dateStr +  '</br>' + smartObject.placeName +  '</a>';
 
      /*   editor.paste(objectUrl);
         editor.update();*/
