93b001bc3f7eb9f73b317cf6870c6f8973fc6c14
Don Bradford <donbrad@hotmail.com>
2016-01-20 17:17:03 -0800

    working on channelView title click

--- ghostgram hybrid/scripts/views/channelViews.js
+++ ghostgram hybrid/scripts/views/channelViews.js
@@ -1084,7 +1084,9 @@ var channelView = {
             var placeUrl = LZString.compressToEncodedURIComponent(placeId);
 
             channelView.onHide();
-            APP.kendo.navigate('#placeView?place=' + placeUrl);
+
+            setTimeout( function () {APP.kendo.navigate('#placeView?place=' + placeUrl);}, 500);
+            //APP.kendo.navigate('#placeView?place=' + placeUrl);
         }
 
 
