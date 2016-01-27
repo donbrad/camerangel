d42fe3dc17ad56911843570a3bb2a50cce9af57e
Don Bradford <donbrad@hotmail.com>
2016-01-27 11:49:00 -0800

    working on placeview display

--- ghostgram hybrid/scripts/views/placeViews.js
+++ ghostgram hybrid/scripts/views/placeViews.js
@@ -1267,7 +1267,7 @@ var placeView = {
         if (photoList !== undefined && photoList.length > 0) {
             for (var p=0; p<photoList.length; p++) {
                 var photo = photoList[p];
-                photo.date = new Date(photo.dateString);
+                photo.date = new Date(photo.updatedAt);
                 ds.add(photo);
             }
             if (notesList !== undefined && notesList.length > 0) {
