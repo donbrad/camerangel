2abedde8850867c4268b4f5a9ced35f1c4ea8971
Don Bradford <donbrad@hotmail.com>
2016-02-13 11:36:41 -0800

    working on inapp search window

--- ghostgram hybrid/scripts/models/mapModel.js
+++ ghostgram hybrid/scripts/models/mapModel.js
@@ -327,6 +327,9 @@ var mapModel = {
     },
 
     reverseGeoCode : function(lat,lng, callback) {
+        lat = mapModel.validNumber(lat);
+        lng = mapModel.validNumber(lng);
+        
         var latlng = new google.maps.LatLng(lat, lng);
 
         mapModel.geocoder.geocode({
