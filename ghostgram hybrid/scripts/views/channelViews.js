923da4d0a447c5d772a8e840a5695afe78033f13
Don Bradford <donbrad@hotmail.com>
2016-02-18 16:14:30 -0800

    adding movie links via inappbrowser

--- ghostgram hybrid/scripts/views/channelViews.js
+++ ghostgram hybrid/scripts/views/channelViews.js
@@ -2159,7 +2159,7 @@ var channelView = {
     },
 
     messageSearchLoad : function (event) {
-		mobileNotify('url: ' + event.url);
+		channelView.searchUrl = event.url;
     },
 
     messageSearchError : function (event) {
@@ -2170,7 +2170,7 @@ var channelView = {
         var exitUrl = event.url;
 
         if (channelView.winRef !== undefined && channelView.winRef !== null) {
-            //channelView.winRef.removeEventListener('loadstart', channelView.messageSearchLoad);
+            channelView.winRef.removeEventListener('loadstart', channelView.messageSearchLoad);
             channelView.winRef.removeEventListener("exit", channelView.messageSearchEnd);
             channelView.winRef = null;
         }
@@ -2188,8 +2188,8 @@ var channelView = {
         }
         channelView.winQuery = '?q='+query;
         channelView.winRef =  window.open(encodeURI(searchUrl), '_blank', 'location=yes');
-     /*   channelView.winRef.addEventListener('loadstart', channelView.messageSearchLoad);
-        channelView.winRef.addEventListener('loaderror', channelView.messageSearchError); */
+        channelView.winRef.addEventListener('loadstart', channelView.messageSearchLoad);
+     /*   channelView.winRef.addEventListener('loaderror', channelView.messageSearchError); */
         channelView.winRef.addEventListener("exit", channelView.messageSearchEnd);
 
     },
