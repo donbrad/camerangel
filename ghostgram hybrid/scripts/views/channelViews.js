22a076f758e67bab199bc01a7bf26d78692b7377
Don Bradford <donbrad@hotmail.com>
2016-02-25 13:47:17 -0800

    working on smart event dates

--- ghostgram hybrid/scripts/views/channelViews.js
+++ ghostgram hybrid/scripts/views/channelViews.js
@@ -1818,7 +1818,7 @@ var channelView = {
             '</span>' +
             '<span class="btnSmart-content">' +
             '<span class="btnSmart-title">' + smartEvent.title + ' </span><br /> ' +
-            '<span class="btnSmart-date">' + dateStr + ' ' + localTime + '</span><br /> ' +
+            '<span class="btnSmart-date">' + dateStr + ' at ' + localTime + '</span><br /> ' +
             '<span class="btnSmart-date">' + placeName + '</span> ' +
             '</span>' +
             '</span></div>';
