e23e015c5f6680004e48aadc89e22caa885bb83b
Don Bradford <donbrad@hotmail.com>
2016-03-04 10:37:45 -0800

    adding ux action stubs to privateNotesView

--- ghostgram hybrid/scripts/views/privateNotesView.js
+++ ghostgram hybrid/scripts/views/privateNotesView.js
@@ -12,11 +12,12 @@
  */
 var privateNotesView = {
     notesDS : null,
+    _titleTagActive: false,
 
 
     onInit : function (e) {
         _preventDefault(e);
-       
+
     },
 
 
@@ -61,6 +62,61 @@ var privateNotesView = {
 
 
     },
+
+    toggleTitleTag : function () {
+
+        if (this._titleTagActive)
+            $('#privateNoteTitleTag').removeClass('hidden');
+        else
+            $('#privateNoteTitleTag').addClass('hidden');
+    },
+
+    noteTitleTag : function (e) {
+        _preventDefault(e);
+
+        this._titleTagActive = !this._titleTagActive;
+        this.toggleTitleTag();
+    },
+
+    noteEditor : function (e) {
+        _preventDefault(e);
+    },
+
+    noteSearch : function (e) {
+        _preventDefault(e);
+    },
+
+    addEvent : function (e) {
+        _preventDefault(e);
+    },
+
+    addMovie : function (e) {
+        _preventDefault(e);
+    },
+
+    addCamera : function (e) {
+        _preventDefault(e);
+    },
+
+    addPhoto : function (e) {
+        _preventDefault(e);
+    },
+
+    addGallery : function (e) {
+        _preventDefault(e);
+    },
+
+    deleteNote : function (e) {
+        _preventDefault(e);
+    },
+
+    editNote : function (e) {
+        _preventDefault(e);
+    },
+
+    shareNote : function (e) {
+        _preventDefault(e);
+    }
 
 
 
