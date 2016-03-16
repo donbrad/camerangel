870c8fda3def6dbc3418156ca84c1b79efe19778
Don Bradford <donbrad@hotmail.com>
2016-03-15 20:54:08 -0700

    adding memberdirectory

--- ghostgram hybrid/scripts/models/memberdirectory.js
+++ ghostgram hybrid/scripts/models/memberdirectory.js
@@ -22,11 +22,38 @@ var memberdirectory = {
         var data = APP.everlive.data(memberdirectory._ggClass);
         data.get(filter)
             .then(function(data){
-                    memberdirectory._id = data.result.id;
+                    if (data.count === 0) {
+                        memberdirectory.create();
+                    } else {
+                        var member = data.result[0];
+                        memberdirectory._id = member.id;
+                    }
+
                 },
                 function(error){
                     mobileNotify("Member Directory Init error : " + JSON.stringify(error));
                 });
+    },
+
+    create : function () {
+        var data = APP.everlive.data(memberdirectory._ggClass);
+
+        var dirObj = {
+            userUUID : userModel.currentUser.userUUID,
+            name : userModel.currentUser.name,
+            alias : userModel.currentUser.alias,
+            phone : userModel.currentUser.phone,
+            email:  userModel.currentUser.email,
+            publicKey: userModel.currentUser.publcKey
+        };
+
+        data.create(dirObj,
+            function(data){
+                var count = data.count;
+            },
+            function(error){
+                mobileNotify("Member Directory Init error : " + JSON.stringify(error));
+            });
     },
 
     update : function () {
