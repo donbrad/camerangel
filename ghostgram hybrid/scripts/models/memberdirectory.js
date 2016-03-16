808926a6ef31cbcc2f04d58df8886fed413d958c
Don Bradford <donbrad@hotmail.com>
2016-03-15 20:20:39 -0700

    adding memberdirectory

---
+++ ghostgram hybrid/scripts/models/memberdirectory.js
@@ -0,0 +1,36 @@
+/**
+ * Created by donbrad on 3/15/16.
+ *
+ * memberdirectory model -- updates and access to ghostgrams member directory
+ *
+ */
+
+
+'use strict';
+
+var memberdirectory = {
+
+    _ggClass : 'memberdirectory',
+    _version : 1,
+
+    init : function () {
+
+    },
+
+    update : function () {
+
+    },
+
+    findMemberByUUID : function (uuid) {
+
+    },
+
+    findMemberByPhone : function (phone) {
+
+    },
+
+    findMemberByEmail : function (email) {
+
+    }
+
+};
