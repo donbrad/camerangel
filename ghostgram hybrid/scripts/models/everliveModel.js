c28951bf388dcade7a8e184672d4b1609bfd75ff
Don Bradford <donbrad@hotmail.com>
2016-03-28 19:25:50 -0700

    working encrypted offline file storage

--- ghostgram hybrid/scripts/models/everliveModel.js
+++ ghostgram hybrid/scripts/models/everliveModel.js
@@ -21,11 +21,11 @@ var everlive = {
     init: function () {
         
        var provider = Everlive.Constants.StorageProvider.FileSystem;
-        if (window.navigator.simulator === undefined) {
+       /* if (window.navigator.simulator === undefined) {
             // Use local storage in the emulator
             provider = Everlive.Constants.StorageProvider.LocalStorage;
         }
-
+*/
 
         APP.everlive = new Everlive({
             appId: 's2fo2sasaubcx7qe',
