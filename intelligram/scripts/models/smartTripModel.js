967e464ca75b064ac42b896b1661c87b6d49adef
Don Bradford <donbrad@hotmail.com>
2016-07-07 14:37:05 -0700

    adding model stubs for smartPlace (intelliplace) and smartTrip (intellitrip).   decided not to do a global rename of all the smart objects

---
+++ intelligram/scripts/models/smartTripModel.js
@@ -0,0 +1,16 @@
+/**
+ * Created by donbrad on 1/20/16.
+ */
+
+'use strict';
+
+var smartTrip = {
+
+    _cloudClass : 'smartTrip',
+    _ggClass : 'smarttrip',
+    _version : 1,
+
+    init : function (e) {
+
+    }
+};
